c5f973629a0a303d2f29615412f08b803994187f
Lucas Boucher <lucas@sublissi.me>
2021-04-09 19:25:15 +0200

    This commit is a design proposal :
    We move the code related to how the discounts should update into the DiscountOffer class itself
    
    That way we get a store class that simply iterates over the discounts and call their respective update methods

--- store.js
+++ store.js
@@ -1,16 +1,60 @@
 const MAX_DISCOUNT_RATE_IN_PERCENT = 50;
 
+function getValueWithinBoundaries(value, minValue, maxValue) {
+  const ceiledValue = Math.min(value, maxValue);
+  return Math.max(ceiledValue, minValue);
+}
+
 export class DiscountOffer {
   constructor(partnerName, expiresIn, discountRateInPercent) {
     this.partnerName = partnerName;
     this.expiresIn = expiresIn;
     this.discountInPercent = discountRateInPercent;
   }
-}
 
-function getValueWithinBoundaries(value, minValue, maxValue) {
-  const ceiledValue = Math.min(value, maxValue);
-  return Math.max(ceiledValue, minValue);
+  updateDiscount() {
+    switch (this.partnerName) {
+      case "Naturalia": {
+        if (this.expiresIn <= 0) {
+          this.discountInPercent += 2;
+        } else {
+          this.discountInPercent += 1;
+        }
+        break;
+      }
+      case "Vinted": {
+        if (this.expiresIn <= 0) {
+          this.discountInPercent = 0;
+        } else if (this.expiresIn <= 5) {
+          this.discountInPercent += 3;
+        } else if (this.expiresIn <= 10) {
+          this.discountInPercent += 2;
+        }
+        break;
+      }
+      case "Ilek": {
+        break;
+      }
+      default: {
+        if (this.expiresIn <= 0) {
+          this.discountInPercent -= 2;
+        } else {
+          this.discountInPercent -= 1;
+        }
+      }
+    }
+    this.discountInPercent = getValueWithinBoundaries(
+      this.discountInPercent,
+      0,
+      MAX_DISCOUNT_RATE_IN_PERCENT
+    );
+  }
+
+  updateExpiration() {
+    if (this.partnerName !== "Ilek") {
+      this.expiresIn -= 1;
+    }
+  }
 }
 
 export class Store {
@@ -20,44 +64,8 @@ export class Store {
 
   updateDiscounts() {
     for (var i = 0; i < this.discountOffers.length; i++) {
-      switch (this.discountOffers[i].partnerName) {
-        case "Naturalia": {
-          if (this.discountOffers[i].expiresIn <= 0) {
-            this.discountOffers[i].discountInPercent += 2;
-          } else {
-            this.discountOffers[i].discountInPercent += 1;
-          }
-          this.discountOffers[i].expiresIn -= 1;
-          break;
-        }
-        case "Vinted": {
-          if (this.discountOffers[i].expiresIn <= 0) {
-            this.discountOffers[i].discountInPercent = 0;
-          } else if (this.discountOffers[i].expiresIn <= 5) {
-            this.discountOffers[i].discountInPercent += 3;
-          } else if (this.discountOffers[i].expiresIn <= 10) {
-            this.discountOffers[i].discountInPercent += 2;
-          }
-          this.discountOffers[i].expiresIn -= 1;
-          break;
-        }
-        case "Ilek": {
-          break;
-        }
-        default: {
-          if (this.discountOffers[i].expiresIn <= 0) {
-            this.discountOffers[i].discountInPercent -= 2;
-          } else {
-            this.discountOffers[i].discountInPercent -= 1;
-          }
-          this.discountOffers[i].expiresIn -= 1;
-        }
-      }
-      this.discountOffers[i].discountInPercent = getValueWithinBoundaries(
-        this.discountOffers[i].discountInPercent,
-        0,
-        MAX_DISCOUNT_RATE_IN_PERCENT
-      );
+      this.discountOffers[i].updateDiscount();
+      this.discountOffers[i].updateExpiration();
     }
 
     return this.discountOffers;
