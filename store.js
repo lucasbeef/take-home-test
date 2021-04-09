358ee28c93060819e680c7a87679e5f7be066257
Lucas Boucher <lucas@sublissi.me>
2021-04-09 18:34:51 +0200

    Refactored the Store.updateDiscount method using a switch case

--- store.js
+++ store.js
@@ -1,3 +1,5 @@
+const MAX_DISCOUNT_RATE_IN_PERCENT = 50;
+
 export class DiscountOffer {
   constructor(partnerName, expiresIn, discountRateInPercent) {
     this.partnerName = partnerName;
@@ -6,59 +8,56 @@ export class DiscountOffer {
   }
 }
 
+function getValueWithinBoundaries(value, minValue, maxValue) {
+  const ceiledValue = Math.min(value, maxValue);
+  return Math.max(ceiledValue, minValue);
+}
+
 export class Store {
   constructor(discountOffers = []) {
     this.discountOffers = discountOffers;
   }
+
   updateDiscounts() {
     for (var i = 0; i < this.discountOffers.length; i++) {
-      if (
-        this.discountOffers[i].partnerName != "Naturalia" &&
-        this.discountOffers[i].partnerName != "Vinted"
-      ) {
-        if (this.discountOffers[i].discountInPercent > 0) {
-          if (this.discountOffers[i].partnerName != "Ilek") {
-            this.discountOffers[i].discountInPercent = this.discountOffers[i].discountInPercent - 1;
-          }
-        }
-      } else {
-        if (this.discountOffers[i].discountInPercent < 50) {
-          this.discountOffers[i].discountInPercent = this.discountOffers[i].discountInPercent + 1;
-          if (this.discountOffers[i].partnerName == "Vinted") {
-            if (this.discountOffers[i].expiresIn < 11) {
-              if (this.discountOffers[i].discountInPercent < 50) {
-                this.discountOffers[i].discountInPercent = this.discountOffers[i].discountInPercent + 1;
-              }
-            }
-            if (this.discountOffers[i].expiresIn < 6) {
-              if (this.discountOffers[i].discountInPercent < 50) {
-                this.discountOffers[i].discountInPercent = this.discountOffers[i].discountInPercent + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.discountOffers[i].partnerName != "Ilek") {
-        this.discountOffers[i].expiresIn = this.discountOffers[i].expiresIn - 1;
-      }
-      if (this.discountOffers[i].expiresIn < 0) {
-        if (this.discountOffers[i].partnerName != "Naturalia") {
-          if (this.discountOffers[i].partnerName != "Vinted") {
-            if (this.discountOffers[i].discountInPercent > 0) {
-              if (this.discountOffers[i].partnerName != "Ilek") {
-                this.discountOffers[i].discountInPercent = this.discountOffers[i].discountInPercent - 1;
-              }
-            }
+      switch (this.discountOffers[i].partnerName) {
+        case "Naturalia": {
+          if (this.discountOffers[i].expiresIn <= 0) {
+            this.discountOffers[i].discountInPercent += 2;
           } else {
-            this.discountOffers[i].discountInPercent =
-              this.discountOffers[i].discountInPercent - this.discountOffers[i].discountInPercent;
+            this.discountOffers[i].discountInPercent += 1;
           }
-        } else {
-          if (this.discountOffers[i].discountInPercent < 50) {
-            this.discountOffers[i].discountInPercent = this.discountOffers[i].discountInPercent + 1;
+          this.discountOffers[i].expiresIn -= 1;
+          break;
+        }
+        case "Vinted": {
+          if (this.discountOffers[i].expiresIn <= 0) {
+            this.discountOffers[i].discountInPercent = 0;
+          } else if (this.discountOffers[i].expiresIn <= 5) {
+            this.discountOffers[i].discountInPercent += 3;
+          } else if (this.discountOffers[i].expiresIn <= 10) {
+            this.discountOffers[i].discountInPercent += 2;
           }
+          this.discountOffers[i].expiresIn -= 1;
+          break;
+        }
+        case "Ilek": {
+          break;
+        }
+        default: {
+          if (this.discountOffers[i].expiresIn <= 0) {
+            this.discountOffers[i].discountInPercent -= 2;
+          } else {
+            this.discountOffers[i].discountInPercent -= 1;
+          }
+          this.discountOffers[i].expiresIn -= 1;
         }
       }
+      this.discountOffers[i].discountInPercent = getValueWithinBoundaries(
+        this.discountOffers[i].discountInPercent,
+        0,
+        MAX_DISCOUNT_RATE_IN_PERCENT
+      );
     }
 
     return this.discountOffers;
